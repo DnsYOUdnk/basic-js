2584ee848f5ed8101f00520b4fb1a5ffb82d2798
Dinis <d.yudenka@yandex.by>
2022-05-02 01:24:35 +0300

    feat: add a solution dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let arr = domains.map(item => item.split('.').reverse()),
+      obj = {};
+      arr.forEach(item => {
+        item.reduce((sum, elem) => {
+          let name = sum + '.' + elem;
+          !obj[name] ? obj[name] = 1 : obj[name] += 1;
+          return name
+        },'')
+      })
+      
+  return obj
 }
 
 module.exports = {
