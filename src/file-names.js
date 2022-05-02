f2380ce613ab507fcdd53dd46b74d710679e46ac
Dinis <d.yudenka@yandex.by>
2022-05-02 15:17:31 +0300

    feat: add a solution

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let arr = [];
+  let obj = {};
+  names.forEach(item => {
+    if(obj[item] == undefined) {
+      obj[item] = 1;
+    } else {
+      obj[item] += 1;
+    }
+    !arr.includes(item) ? arr.push(item) : arr.push(item + `(${obj[item]-1 === 0 ? 1 : obj[item]-1})`);
+  })
+  return arr
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let arrNum = [...arr],
+      oneM = [],
+      id = 0;
+      arrNum = arrNum.sort((a,b) => a-b).filter(item => item !== -1)
+      oneM = arr.map((item, index) => {
+        if(item == -1) {
+          id++
+          return item
+        }
+        return arrNum[index-id]
+      })
+  return oneM
 }
 
 module.exports = {
